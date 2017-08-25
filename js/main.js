f767a2b6630ff5796570eea5bf683c7db0c24983
Haokun Wang <terryneverstop@gmail.com>
2017-08-24 18:46:43 -0700

    8/24/2017 update3

--- js/main.js
+++ js/main.js
@@ -65,8 +65,24 @@ function scaleBannerVideoSize(element){
     });
 }
 
-// // scroll to middle
-// var targetOffset = $(thisTarget).offset().top-200;
+
+// Smooth scroll this mother!
+
+// Do it when someone clicks a nav link
+$('.home-hero-content a').on('click', function(e) {
+  // prevent the standard link operation on click
+  e.preventDefault();
+  // use the href of the link to identify what
+  // section to scroll to
+  var thisTarget = $(this).attr('href');
+  // get that section's top offset
+  var targetOffset = $(thisTarget).offset().top -80;
+  // use jQuery.animate() to animate the body's
+  // scrollTop to the targetOffest
+  $('body').animate({
+    scrollTop: targetOffset
+  }, 600);
+});
 
 
 // ---------- home slider
